72584f52d47141d367ddecd3db88f64cbe8ccf5a
Stan Vespie <svespie@outlook.com>
2016-03-14 01:32:32 -0400

    Added a unit test project with tests covering basic input validation and confirming the implementation for each formula.

---
+++ OneRepMax/OneRepMax.Tests/CalculateTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OneRepMax.Tests
+{
+    [TestClass]
+    public class CalculateTests
+    {
+        [TestMethod]
+        public void ShouldNotThrowAnExceptionWithWeightGreaterThan1()
+        {
+            try
+            {
+                Calculate.OneRepMax(135, 5, Formula.Average);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowAnExceptionWithRepsEqual1()
+        {
+            try
+            {
+                Calculate.OneRepMax(135, 1, Formula.Average);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowAnExceptionWithRepsEqual10()
+        {
+            try
+            {
+                Calculate.OneRepMax(135, 10, Formula.Average);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldThrowAnExceptionWhenWeightIsLessThan1()
+        {
+            Calculate.OneRepMax(0.999, 5, Formula.Average);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldThrowAnExceptionWhenRepsIsLessThan1()
+        {
+            Calculate.OneRepMax(135, 0, Formula.Average);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ShouldThrowAnExceptionWhenRepsIsGreaterThan10()
+        {
+            Calculate.OneRepMax(135, 11, Formula.Average);
+        }
+    }
+}
