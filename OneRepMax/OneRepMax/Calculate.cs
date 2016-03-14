2ee86ad59496d67381698c2c3eac5d51b8626c5f
Stan Vespie <svespie@outlook.com>
2016-03-14 00:57:45 -0400

    A few more updates to the XML comments.

--- OneRepMax/OneRepMax/Calculate.cs
+++ OneRepMax/OneRepMax/Calculate.cs
@@ -68,82 +68,82 @@ namespace OneRepMax
         }
 
         /// <summary>
-        /// Estimates a 1RM based on the Wathan formula.
+        /// Estimates a 1RM based on the Wathan formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Wathan formula.
         /// </returns>
         private static double Wathan(double weight, int reps) => 100.0 * weight / (48.8 + 53.8 * Math.Pow(Math.E, -0.075 * reps));
 
         /// <summary>
-        /// Estimates a 1RM based on the O'Conner formula.
+        /// Estimates a 1RM based on the O'Conner formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the O'Conner formula.
         /// </returns>
         private static double OConner(double weight, int reps) => weight * (1.0 + 0.025 * (double)reps);
 
         /// <summary>
-        /// Estimates a 1RM based on the Mayhew formula.
+        /// Estimates a 1RM based on the Mayhew formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Mayhew formula.
         /// </returns>
         private static double Mayhew(double weight, int reps) => 100.0 * weight / (52.2 + 41.9 * Math.Pow(Math.E, -0.055 * reps));
 
         /// <summary>
-        /// Estimates a 1RM based on the Lombardi formula.
+        /// Estimates a 1RM based on the Lombardi formula.This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Lombardi formula.
         /// </returns>
         private static double Lombardi(double weight, int reps) => weight * Math.Pow(reps, 0.1);
 
         /// <summary>
-        /// Estimates a 1RM based on the Lander formula.
+        /// Estimates a 1RM based on the Lander formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Lander formula.
         /// </returns>
         private static double Lander(double weight, int reps) => 100.0 * weight / (101.3 - 2.67123 * reps);
 
         /// <summary>
-        /// Estimates a 1RM based on the Epley formula.
+        /// Estimates a 1RM based on the Epley formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Epley formula.
         /// </returns>
         private static double Epley(double weight, int reps) => weight * reps / 30.0 + weight;
 
         /// <summary>
-        /// Estimates a 1RM based on the Brzycki formula.
+        /// Estimates a 1RM based on the Brzycki formula. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on the Brzycki formula.
         /// </returns>
         private static double Brzycki(double weight, int reps) => weight * (36.0 / (37.0 - Convert.ToDouble(reps)));
 
         /// <summary>
-        /// Estimates a 1RM based on an average of all the forumulas implemented within this library.
+        /// Estimates a 1RM based on an average of all the forumulas implemented within this library. This method assumes valid input.
         /// </summary>
-        /// <param name="weight">The weight used in testing. Units are not necessary. The output will be in the same units as this value. Valid values are &gt;= 1.</param>
-        /// <param name="reps">The number of repetitions performed. A range of 1 through 9 is often considered to be most accurate. Valid values are &gt;= 1 and &lt;=10.</param>
+        /// <param name="weight">Double value representing the weight used in testing. Valid values are &gt;= 1.</param>
+        /// <param name="reps">Int value representing the number of repetitions performed. Valid values are &gt;= 1 and &lt;=10.</param>
         /// <returns>
-        /// Returns an estimated 1RM max rounded to two decimals if the input is valid. An exception is thrown if the input is invalid.
+        /// Returns an estimated 1RM based on an average of all the forumulas implemented within this library.
         /// </returns>
         private static double Average(double weight, int reps) => Avg(Brzycki(weight, reps), Epley(weight, reps), Lander(weight, reps), Lombardi(weight, reps), Mayhew(weight, reps), OConner(weight, reps), Wathan(weight, reps));
 
